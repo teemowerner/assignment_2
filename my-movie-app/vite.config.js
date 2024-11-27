beb523b213c29d973731cbbd4ade3d7fc0dcc31c
teemowerner <dong338338@naver.com>
2024-11-27 19:38:06 +0900

    Add dist folder to repository

--- my-movie-app/vite.config.js
+++ my-movie-app/vite.config.js
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +11,12 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  build: {
+    outDir: 'dist', // 빌드 결과물이 저장될 디렉터리
+    assetsDir: 'assets', // 정적 파일이 저장될 하위 디렉터리
+    emptyOutDir: true, // 기존 빌드 파일 제거
+  },
+});
